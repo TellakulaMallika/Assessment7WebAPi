59aec5aca115eac438b425a3c87f9b5e0e9bfc5a
T.Mallika <tellakulamallika@gmailc.om>
2024-02-07 16:13:01 -0700

    firstcommit

---
+++ WebAPIMovieDatabase/WebAPIMovieDatabase/Migrations/20240207224303_MovMig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPIMovieDatabase.Migrations
+{
+    public partial class MovMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movie",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StatCast = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Director = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Producer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReleaseDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movie", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movie");
+        }
+    }
+}
---
+++ WebAPIMovieDatabase/WebAPIMovieDatabase/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPIMovieDatabase.Models
+{
+    [Table("Movie")]
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(100)]
+        
+        
+        public string StatCast { get; set; }
+        public string Director { get; set; }
+        public string Producer { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
